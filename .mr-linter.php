5b6e25b56be46714851fc7bf79ebdc3b6ab1a3d1
ArtARTs36 <temicska99@mail.ru>
2021-12-08 06:25:30 +0300

    require mr-lint

---
+++ .mr-linter.php
@@ -0,0 +1,32 @@
+<?php
+
+use ArtARTs36\MergeRequestLinter\Ci\Credentials\Token;
+use ArtARTs36\MergeRequestLinter\Ci\System\GithubActions;
+use ArtARTs36\MergeRequestLinter\Ci\System\GitlabCi;
+use ArtARTs36\MergeRequestLinter\Contracts\Environment;
+use ArtARTs36\MergeRequestLinter\Rule\HasAnyLabelsOfRule;
+use ArtARTs36\MergeRequestLinter\Rule\HasAnyLabelsRule;
+use ArtARTs36\MergeRequestLinter\Rule\DescriptionNotEmptyRule;
+use ArtARTs36\MergeRequestLinter\Rule\TitleStartsWithAnyPrefixRule;
+
+return [
+    'rules' => [
+        new DescriptionNotEmptyRule(),
+        new HasAnyLabelsRule(),
+        HasAnyLabelsOfRule::make([
+            'Feature',
+            'Bug',
+            'Docs',
+        ]),
+        TitleStartsWithAnyPrefixRule::make([
+            '[Feature]',
+            '[Bug]',
+            '[Docs]',
+            '[CI]',
+        ]),
+    ],
+    'credentials' => [
+        GitlabCi::class => new Token(getenv('MR_LINTER_GITLAB_HTTP_TOKEN')),
+    ],
+    'http_client' => fn (string $ciName, Environment $environment, string $ciSystemClass) => new \GuzzleHttp\Client(),
+];
