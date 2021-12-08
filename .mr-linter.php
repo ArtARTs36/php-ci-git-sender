51a5fa4964cfae83172e991c5034fe04e45c90d5
ArtARTs36 <temicska99@mail.ru>
2021-12-08 06:33:24 +0300

    change credentials

--- .mr-linter.php
+++ .mr-linter.php
@@ -2,7 +2,6 @@
 
 use ArtARTs36\MergeRequestLinter\Ci\Credentials\Token;
 use ArtARTs36\MergeRequestLinter\Ci\System\GithubActions;
-use ArtARTs36\MergeRequestLinter\Ci\System\GitlabCi;
 use ArtARTs36\MergeRequestLinter\Contracts\Environment;
 use ArtARTs36\MergeRequestLinter\Rule\HasAnyLabelsOfRule;
 use ArtARTs36\MergeRequestLinter\Rule\HasAnyLabelsRule;
@@ -26,7 +25,7 @@ return [
         ]),
     ],
     'credentials' => [
-        GitlabCi::class => new Token(getenv('MR_LINTER_GITLAB_HTTP_TOKEN')),
+        GithubActions::class => new Token(getenv('MR_LINTER_GITHUB_HTTP_TOKEN')),
     ],
     'http_client' => fn (string $ciName, Environment $environment, string $ciSystemClass) => new \GuzzleHttp\Client(),
 ];
